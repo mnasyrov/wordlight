dad269c938cacd02f344f9c92fe9195908176aa4
streambot <streambot@d6100325-a522-3951-b879-fda9d31c7e15>
2010-06-23 10:58:49 +0000

    Initial commit

---
+++ trunk/TextPaneWindow.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+using EnvDTE;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace WordLight
+{
+	public class TextPaneWindow : NativeWindow
+	{
+		#region WinProc Messages
+
+		private const int WM_KEYDOWN = 0x0100;
+		private const int WM_KEYUP = 0x101;
+		private const int WM_LBUTTONUP = 0x202;
+		private const int WM_RBUTTONUP = 0x205;
+		private const int WM_MBUTTONUP = 0x208;
+		private const int WM_XBUTTONUP = 0x20C;
+		private const int WM_LBUTTONDOWN = 0x201;
+		private const int WM_RBUTTONDOWN = 0x204;
+		private const int WM_MBUTTONDOWN = 0x207;
+		private const int WM_XBUTTONDOWN = 0x20B;
+		private const int WM_LBUTTONDBLCLK = 0x0203;
+		private const int WM_MBUTTONDBLCLK = 0x0209;
+		private const int WM_RBUTTONDBLCLK = 0x0206;
+		private const int WM_XBUTTONDBLCLK = 0x020D;
+		private const int WM_PARENTNOTIFY = 0x0210;
+		private const int WM_PAINT = 0x000F;
+
+		#endregion
+
+		//private struct ScrollInfo
+		//{
+		//    public int MinUnit;
+		//    public int MaxUnit;
+		//    public int VisibleUnit;
+		//    public int FirstVisibleUnit;
+		//}
+
+		//private enum ScrollBarTypes : int
+		//{
+		//    Horizontal = 0,
+		//    Vertical = 1
+		//}
+
+        private string _previousSelectedText;
+		public event EventHandler OnInput;
+
+		private List<WordMark> _marks = new List<WordMark>();
+
+		//private ScrollInfo _horizontalScroll;
+		//private ScrollInfo _verticalScroll;
+
+        private IVsTextView _view;
+        private TextPane _pane;
+		
+		//public int ScrollHoriz
+		//{
+		//    get { return _horizontalScroll.FirstVisibleUnit; }
+		//}
+
+		//public int ScrollVert
+		//{
+		//    get { return _verticalScroll.FirstVisibleUnit; }
+		//}
+
+        public TextPaneWindow(IntPtr paneHandle, TextPane pane, IVsTextView view)
+		{
+            _pane = pane;
+            _view = view;
+			AssignHandle(paneHandle);
+		}
+
+		protected override void WndProc(ref Message m)
+		{
+			base.WndProc(ref m);
+
+			switch (m.Msg)
+			{
+				case WM_KEYUP:
+				case WM_KEYDOWN:
+				case WM_LBUTTONUP:
+				case WM_RBUTTONUP:
+				case WM_MBUTTONUP:
+				case WM_XBUTTONUP:
+				case WM_LBUTTONDOWN:
+				case WM_MBUTTONDOWN:
+				case WM_RBUTTONDOWN:
+				case WM_XBUTTONDOWN:
+				case WM_LBUTTONDBLCLK:
+				case WM_MBUTTONDBLCLK:
+				case WM_RBUTTONDBLCLK:
+				case WM_XBUTTONDBLCLK:
+					HandleUserInput();
+					break;
+
+				case WM_PAINT:
+					Paint();
+					break;
+			}
+		}
+
+		private void Paint()
+		{
+			//_horizontalScroll = GetScrollInfo(ScrollBarTypes.Horizontal);
+			//_verticalScroll = GetScrollInfo(ScrollBarTypes.Vertical);
+
+			using (Graphics g = Graphics.FromHwnd(this.Handle))
+			{
+				System.Drawing.Drawing2D.GraphicsContainer cont = g.BeginContainer();
+
+				// Draw marks
+				foreach (WordMark mark in _marks)
+				{
+					mark.Draw(g, _view);
+				}
+
+				g.EndContainer(cont);
+			}
+		}
+
+		public void AddMark(EditPoint start, EditPoint end)
+		{
+			var mark = new WordMark();
+			mark.StartLine = start.Line;
+			mark.StartLineIndex = start.LineCharOffset;
+			mark.EndLine = end.Line;
+			mark.EndLineIndex = end.LineCharOffset;
+
+			_marks.Add(mark);
+		}
+
+		public void ClearMarks()
+		{
+			_marks.Clear();
+		}
+
+		public void Refresh()
+		{
+			InvalidateRect(Handle, IntPtr.Zero, true);
+			UpdateWindow(Handle);
+		}
+
+		[DllImport("user32.dll", SetLastError = false)]
+		private static extern bool UpdateWindow(IntPtr hWnd);
+
+		[DllImport("user32.dll", SetLastError = false)]
+		private static extern bool InvalidateRect(IntPtr hWnd, IntPtr rect, bool erase);
+
+		//private ScrollInfo GetScrollInfo(ScrollBarTypes iBar)
+		//{
+		//    ScrollInfo data = new ScrollInfo();
+			
+		//    if (View != null)
+		//    {
+		//        View.GetScrollInfo((int)iBar, out data.MinUnit, out data.MaxUnit, out data.VisibleUnit, out data.FirstVisibleUnit);
+		//    }
+			
+		//    return data;
+		//}
+
+		private void HandleUserInput()
+		{
+			EventHandler evt = OnInput;
+			if (evt != null) evt(this, EventArgs.Empty);
+
+            //if (_applicationObject == null || _applicationObject.ActiveDocument == null)
+            //    return;
+
+            TextDocument textDoc = _pane.Window.Document.Object("TextDocument") as TextDocument;
+            if (textDoc != null)
+            {
+                string selectedText = GetSelectedText(textDoc);
+
+                if (selectedText != _previousSelectedText)
+                {
+                    _previousSelectedText = selectedText;
+                    ProcessSelectedText(textDoc, selectedText);
+                }
+            }
+		}
+
+        private string GetSelectedText(TextDocument textDoc)
+        {
+            string text = string.Empty;
+
+            TextSelection selection = textDoc.Selection as TextSelection;
+            if (selection != null)
+            {
+                text = selection.Text;
+            }
+
+            if (text == null)
+                text = string.Empty;
+            else
+                text = text.Trim();
+
+            return text;
+        }
+
+        private void ProcessSelectedText(TextDocument textDoc, string text)
+        {
+            ClearMarks();
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                bool result;
+                EditPoint searchStart = textDoc.StartPoint.CreateEditPoint();
+                EditPoint searchEnd = textDoc.EndPoint.CreateEditPoint();
+                TextRanges ranges = null;
+
+                do
+                {
+                    result = searchStart.FindPattern(text, (int)vsFindOptions.vsFindOptionsNone, ref searchEnd, ref ranges);
+                    if (result)
+                    {
+                        AddMark(searchStart, searchEnd);
+                    }
+                    searchStart = searchEnd;
+                } while (result);
+            }
+
+            Refresh();
+
+            //int highlightID;
+            //Guid highlightGuid = ...; // your highlighted text style guid
+            //textManager.GetRegisteredMarkerTypeID(ref highlightGuid, out highlightID);
+
+            //// highlighting text block in the active view
+            //IVsTextView view;
+            //int result = textManager.GetActiveView(0, null, out view);
+            //IVsTextLines buffer;
+            //view.GetBuffer(out buffer);
+            //buffer.CreateLineMarker(highlightID, startLine, startColumn, endLine, endColumn, null, null);
+        }
+	}
+}
---
+++ trunk/WindowWatcher.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+using EnvDTE;
+using EnvDTE80;
+using Extensibility;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace WordLight
+{
+    public class WindowWatcher : IDisposable
+    {
+        private DTE2 _applicationObject;
+        private WindowEvents _windowEvents;
+        private IntPtr _mainWindowHandle;
+        private IVsTextManager _textManager;
+        private Dictionary<string, TextPaneWindow> _textPaneWindows;
+
+		public event EventHandler OnTextPaneInput;
+
+        [DllImport("User32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string className, string windowName);
+
+        public WindowWatcher(DTE2 application)
+        {
+            _applicationObject = application;
+            _mainWindowHandle = (IntPtr)application.MainWindow.HWnd;
+
+            _textPaneWindows = new Dictionary<string, TextPaneWindow>();
+
+            // Get IVsTextManager
+            var serviceProvider = _applicationObject as Microsoft.VisualStudio.OLE.Interop.IServiceProvider;
+
+            Guid SID = typeof(SVsTextManager).GUID;
+            Guid IID = typeof(IVsTextManager).GUID;
+            IntPtr output;
+            serviceProvider.QueryService(ref SID, ref IID, out output);
+
+            _textManager = (IVsTextManager)Marshal.GetObjectForIUnknown(output);
+
+            // Catch window events
+            _windowEvents = _applicationObject.Events.get_WindowEvents(null);
+
+            _windowEvents.WindowCreated += new _dispWindowEvents_WindowCreatedEventHandler(OnWindowCreated);
+            _windowEvents.WindowClosing += new _dispWindowEvents_WindowClosingEventHandler(OnWindowClosing);
+        }
+
+        private void OnWindowCreated(Window win)
+        {
+            TextWindow textWin = win.Object as TextWindow;
+            if (textWin != null)
+            {
+                AddTextPane(textWin.ActivePane);
+            }
+        }
+
+        private TextPaneWindow AddTextPane(TextPane pane)
+        {
+            TextPaneWindow paneWindow = null;
+
+            if (pane != null)
+            {
+                IntPtr paneHandle = GetVsTextEditPane(_mainWindowHandle, pane.Window);
+                if (paneHandle != IntPtr.Zero)
+                {
+                    IVsTextView view;
+                    _textManager.GetActiveView(1, null, out view);
+
+                    paneWindow = new TextPaneWindow(paneHandle, pane, view);
+					paneWindow.OnInput += new EventHandler(paneWindow_OnInput);
+
+                    _textPaneWindows.Add(pane.Window.Caption, paneWindow);
+                }
+            }
+
+            return paneWindow;
+        }
+
+        private void OnWindowClosing(Window Window)
+        {
+            TextWindow textWin = Window.Object as TextWindow;
+            if (textWin != null)
+            {
+                TextPane pane = textWin.ActivePane;
+                if (_textPaneWindows.ContainsKey(pane.Window.Caption))
+                {
+                    TextPaneWindow paneWindow = _textPaneWindows[pane.Window.Caption];
+                    _textPaneWindows.Remove(pane.Window.Caption);
+
+					paneWindow.OnInput -= paneWindow_OnInput;
+                    paneWindow.ReleaseHandle();
+                }
+            }
+        }
+
+        public TextPaneWindow GetPaneWindow(TextPane pane)
+        {
+            TextPaneWindow paneWindow = null;
+
+            if (pane != null)
+            {
+                if (_textPaneWindows.ContainsKey(pane.Window.Caption))
+                {
+                    paneWindow = _textPaneWindows[pane.Window.Caption];
+                }
+            }
+
+            return paneWindow;
+        }
+
+        private static IntPtr GetVsTextEditPane(IntPtr _mainWindowHandle, Window wnd)
+        {
+            IntPtr pane = IntPtr.Zero;
+
+            if (wnd != null)
+            {
+                string caption = wnd.Caption;
+
+                IntPtr hwndParent = FindWindowEx(_mainWindowHandle, IntPtr.Zero, null, caption); // top level that we can id
+                // This is hackish since we have no easier way to do this
+                hwndParent = FindWindowEx(hwndParent, IntPtr.Zero, null, null);  
+                hwndParent = FindWindowEx(hwndParent, IntPtr.Zero, null, null); // an intermediate window
+                pane = FindWindowEx(hwndParent, IntPtr.Zero, null, null); // This is the VsTextEditPane item
+            }
+
+            return pane;
+        }
+
+        public void Dispose()
+        {
+            if (_windowEvents != null)
+            {
+                _windowEvents.WindowCreated -= new _dispWindowEvents_WindowCreatedEventHandler(OnWindowCreated);
+                _windowEvents.WindowClosing -= new _dispWindowEvents_WindowClosingEventHandler(OnWindowClosing);
+            }
+
+            foreach (TextPaneWindow win in _textPaneWindows.Values)
+            {
+                win.ReleaseHandle();
+            }
+        }
+
+		private void paneWindow_OnInput(object sender, EventArgs e)
+		{
+			EventHandler evt = OnTextPaneInput;
+			if (evt != null) evt(this, EventArgs.Empty);
+		}
+    }
+}
