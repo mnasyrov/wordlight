f46b96c2963b486ecdfe46ac2bc23f6a18465481
streambot <streambot@d6100325-a522-3951-b879-fda9d31c7e15>
2010-06-25 18:08:04 +0000

    Small improvement for a drawing

--- trunk/WordMark.cs
+++ trunk/WordMark.cs
@@ -1,5 +1,6 @@
 ﻿using System.Drawing;
 
+using EnvDTE;
 using Microsoft.VisualStudio.OLE.Interop;
 using Microsoft.VisualStudio.TextManager.Interop;
 
@@ -7,39 +8,44 @@ namespace WordLight
 {
 	public class WordMark
 	{
-		public int StartLine;
-		public int StartLineIndex;
-		public int EndLine;
-		public int EndLineIndex;
+		public int _startLine;
+		public int _startLineIndex;
+		public int _endLine;
+		public int _endLineIndex;
+
+		private POINT[] _startPoint = new POINT[1];
+		private POINT[] _endPoint = new POINT[1];
+
+		private int _lineHeight;
+
+		public WordMark(int lineHeight, EditPoint start, EditPoint end)
+		{
+			_lineHeight = lineHeight;
+
+			_startLine = start.Line - 1;
+			_startLineIndex = start.LineCharOffset - 1;
+			_endLine = end.Line - 1;
+			_endLineIndex = end.LineCharOffset - 1;
+		}
 
 		public void Draw(Graphics g, IVsTextView view)
 		{
-			int lineHeight;
-			view.GetLineHeight(out lineHeight);
+			view.GetPointOfLineColumn(_startLine, _startLineIndex, _startPoint);
+			view.GetPointOfLineColumn(_endLine, _endLineIndex, _endPoint);
 
-			POINT[] startPoint = new POINT[1];
-			view.GetPointOfLineColumn(
-				StartLine - 1,
-				StartLineIndex - 1,
-				startPoint
-			);
+			bool isVisible =
+				g.VisibleClipBounds.Left <= _endPoint[0].x && _startPoint[0].x <= g.VisibleClipBounds.Right
+				&& g.VisibleClipBounds.Top <= _endPoint[0].y && _startPoint[0].y <= g.VisibleClipBounds.Bottom;
 
-			POINT[] endPoint = new POINT[1];
-			view.GetPointOfLineColumn(
-				EndLine - 1,
-				EndLineIndex - 1,
-				endPoint
-			);
-
-			int height = endPoint[0].y - startPoint[0].y + lineHeight;
-			int width = endPoint[0].x - startPoint[0].x;
-
-			int x = startPoint[0].x;
-			int y = startPoint[0].y;
-
-			using (Pen greenPen = new Pen(Color.Lime, 1))
+			if (isVisible)
 			{
-				g.DrawRectangle(greenPen, x, y, width, height);
+				int height = _endPoint[0].y - _startPoint[0].y + _lineHeight;
+				int width = _endPoint[0].x - _startPoint[0].x;
+
+				int x = _startPoint[0].x;
+				int y = _startPoint[0].y;
+
+				g.DrawRectangle(Pens.Lime, x, y, width, height);
 			}
 		}
 	}
