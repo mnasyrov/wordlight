31056566e7991fdfde3c09258615bbe1e000ab1a
streambot <streambot@d6100325-a522-3951-b879-fda9d31c7e15>
2010-07-16 05:51:41 +0000

    + Realized customizing of a outline color for search marks. * Started optimization of drawing marks.

--- trunk/SettingsForm.Designer.cs
+++ trunk/SettingsForm.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WordLight.Controls
+﻿namespace WordLight
 {
     partial class SettingsForm
     {
@@ -30,14 +30,13 @@
         {
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.markColorPreview = new System.Windows.Forms.Button();
-            this.btnMarkColor = new System.Windows.Forms.Button();
+            this.Properties = new System.Windows.Forms.PropertyGrid();
             this.SuspendLayout();
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(226, 240);
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(226, 340);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 0;
@@ -47,43 +46,21 @@
             // 
             // btnCancel
             // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(307, 240);
+            this.btnCancel.Location = new System.Drawing.Point(307, 340);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 1;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // label1
+            // Properties
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 46);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Search mark color";
-            // 
-            // markColorPreview
-            // 
-            this.markColorPreview.BackColor = System.Drawing.Color.Brown;
-            this.markColorPreview.Enabled = false;
-            this.markColorPreview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.markColorPreview.Location = new System.Drawing.Point(126, 41);
-            this.markColorPreview.Name = "markColorPreview";
-            this.markColorPreview.Size = new System.Drawing.Size(23, 23);
-            this.markColorPreview.TabIndex = 3;
-            this.markColorPreview.UseVisualStyleBackColor = false;
-            // 
-            // btnMarkColor
-            // 
-            this.btnMarkColor.Location = new System.Drawing.Point(169, 41);
-            this.btnMarkColor.Name = "btnMarkColor";
-            this.btnMarkColor.Size = new System.Drawing.Size(75, 23);
-            this.btnMarkColor.TabIndex = 4;
-            this.btnMarkColor.Text = "Select...";
-            this.btnMarkColor.UseVisualStyleBackColor = true;
-            this.btnMarkColor.Click += new System.EventHandler(this.btnMarkColor_Click);
+            this.Properties.Location = new System.Drawing.Point(12, 12);
+            this.Properties.Name = "Properties";
+            this.Properties.Size = new System.Drawing.Size(370, 316);
+            this.Properties.TabIndex = 5;
             // 
             // SettingsForm
             // 
@@ -91,10 +68,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(394, 275);
-            this.Controls.Add(this.btnMarkColor);
-            this.Controls.Add(this.markColorPreview);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(394, 375);
+            this.Controls.Add(this.Properties);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -104,7 +79,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "WordLight";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -112,8 +86,6 @@
 
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button markColorPreview;
-        private System.Windows.Forms.Button btnMarkColor;
+        private System.Windows.Forms.PropertyGrid Properties;
     }
 }
