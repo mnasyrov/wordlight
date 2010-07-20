8cbede54f4111692bf486c8b6816e26c184e0d82
streambot <streambot@d6100325-a522-3951-b879-fda9d31c7e15>
2010-07-20 08:32:30 +0000

    Making delayed global search and filtering non-visible search marks.

---
+++ branches/DelayedSearch/EventAdapters/ViewScrollInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace WordLight.EventAdapters
+{
+	public struct ViewScrollInfo : IEquatable<ViewScrollInfo>
+	{
+		public int bar;
+		public int minUnit;
+		public int maxUnit;
+		public int visibleUnits;
+		public int firstVisibleUnit;
+
+		public static ViewScrollInfo Empty = new ViewScrollInfo()
+		{
+			bar = 0,
+			firstVisibleUnit = 0,
+			maxUnit = 0,
+			minUnit = 0,
+			visibleUnits = 0
+		};
+
+		public static ViewScrollInfo CreateByView(IVsTextView view, int iBar)
+		{
+			ViewScrollInfo info = new ViewScrollInfo();
+			info.bar = iBar;
+			view.GetScrollInfo(iBar, out info.minUnit, out info.maxUnit, out info.visibleUnits, out info.firstVisibleUnit);
+			return info;
+		}
+
+		public bool Equals(ViewScrollInfo other)
+		{
+			return
+				bar == other.bar &&
+				minUnit == other.minUnit &&
+				maxUnit == other.maxUnit &&
+				visibleUnits == other.visibleUnits &&
+				firstVisibleUnit == other.firstVisibleUnit;
+		}
+	}
+}
---
+++ branches/DelayedSearch/SearchMark.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+
+using EnvDTE;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace WordLight
+{
+	public struct SearchMark
+	{
+		private int _startLine;
+        private int _startLineIndex;
+        private int _endLine;
+        private int _endLineIndex;
+
+        public SearchMark(EditPoint start, EditPoint end)
+		{
+			_startLine = start.Line - 1;
+			_startLineIndex = start.LineCharOffset - 1;
+			_endLine = end.Line - 1;
+			_endLineIndex = end.LineCharOffset - 1;
+		}
+
+        public Rectangle GetVisibleRectangle(IVsTextView view, RectangleF visibleClipBounds, int lineHeight)
+		{
+			Point startPoint = view.GetPointOfLineColumn(_startLine, _startLineIndex);
+			Point endPoint = view.GetPointOfLineColumn(_endLine, _endLineIndex);
+			
+            bool isVisible =
+				visibleClipBounds.Left <= endPoint.X && startPoint.X <= visibleClipBounds.Right
+				&& visibleClipBounds.Top <= endPoint.Y && startPoint.Y <= visibleClipBounds.Bottom;
+
+            if (isVisible)
+            {
+                int height = endPoint.Y - startPoint.Y + lineHeight;
+                int width = endPoint.X - startPoint.X;
+
+                int x = startPoint.X;
+                int y = startPoint.Y;
+
+                return new Rectangle(x, y, width, height);
+            }
+
+            return Rectangle.Empty;
+		}
+	}
+}
