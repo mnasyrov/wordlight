9534111e34f1f1a9d136ad9dab8389361da55c69
streambot@gmail.com <streambot@gmail.com@d6100325-a522-3951-b879-fda9d31c7e15>
2011-05-23 04:27:57 +0000

    Added an empty package to WordLight2010.

---
+++ branches/vs2010/src/WordLight2010/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace MikhailNasyrov.WordLightPackage
+{
+    static class GuidList
+    {
+        public const string guidWordLightPackagePkgString = "22cd2dc2-d883-46bc-9304-c43a1aaf9c5f";
+        public const string guidWordLightPackageCmdSetString = "6c689197-b42e-4045-8230-9e09996146bd";
+
+        public static readonly Guid guidWordLightPackageCmdSet = new Guid(guidWordLightPackageCmdSetString);
+    };
+}
---
+++ branches/vs2010/src/WordLight2010/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace MikhailNasyrov.WordLightPackage
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidMyCommand =        0x100;
+
+
+    };
+}
