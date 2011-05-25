0744f574172e47869995a8006ccbfa95314a9713
streambot@gmail.com <streambot@gmail.com@d6100325-a522-3951-b879-fda9d31c7e15>
2011-05-25 08:16:43 +0000

    Registered IDE commands for freezing.

--- branches/vs2010/src/WordLight2010/WordLightPackage.cs
+++ branches/vs2010/src/WordLight2010/WordLightPackage.cs
@@ -18,18 +18,6 @@ using WordLight.Settings;
 
 namespace WordLight2010
 {
-	/// <summary>
-	/// This is the class that implements the package exposed by this assembly.
-	///
-	/// The minimum requirement for a class to be considered a valid package for Visual Studio
-	/// is to implement the IVsPackage interface and register itself with the shell.
-	/// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-	/// to do it: it derives from the Package class that provides the implementation of the 
-	/// IVsPackage interface and uses the registration attributes defined in the framework to 
-	/// register itself and its components with the shell.
-	/// </summary>
-	// This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-	// a package.
 	[PackageRegistration(UseManagedResourcesOnly = true)]
 	// This attribute is used to register the informations needed to show the this package
 	// in the Help/About dialog of Visual Studio.
@@ -41,11 +29,6 @@ namespace WordLight2010
 	[Guid(GuidList.guidWordLightPackagePkgString)]
 	public sealed class WordLightPackage : Package, IVsShellPropertyEvents
 	{
-		private const string Freeze1CommandName = "Freeze1";
-		private const string Freeze2CommandName = "Freeze2";
-		private const string Freeze3CommandName = "Freeze3";
-		private const string EnableLogCommandName = "EnableLog";
-
 		private WindowWatcher _watcher;
 		private uint _cookie;
 		private DTE _dte;
@@ -110,12 +93,11 @@ namespace WordLight2010
 
 		protected override void Dispose(bool disposing)
 		{
-			if (_watcher != null)
+			if (disposing && _watcher != null)
 			{
 				_watcher.Dispose();
+				_watcher = null;
 			}
-
-			RemoveCommands();
 
 			base.Dispose(disposing);
 		}
@@ -127,12 +109,10 @@ namespace WordLight2010
 
 			try
 			{
-				AddinSettings.Instance.Load(
-					new RegistrySettingRepository(UserRegistryRoot.CreateSubKey("WordLight"))
-					//new VsSettingRepository(_application.Globals, "WordLight")
-				);
+				var settingsRepository = new RegistrySettingRepository(UserRegistryRoot.CreateSubKey("WordLight"));
+				//var settingsRepository = new VsSettingRepository(_application.Globals, "WordLight");
 
-				RegisterMenuCommands();
+				AddinSettings.Instance.Load(settingsRepository);
 
 				RegisterCommands();
 
@@ -146,22 +126,25 @@ namespace WordLight2010
 			}
 		}
 
-		private void RegisterMenuCommands()
+		private void RegisterCommands()
 		{
-			// Add our command handlers for menu (commands must exist in the .vsct file)
+			var settingsCmdId = new CommandID(GuidList.guidWordLightPackageCmdSet, PkgCmdIDList.cmdidSettings);
+			var freeze1CmdId = new CommandID(GuidList.guidWordLightPackageCmdSet, PkgCmdIDList.cmdidFreezeSearch1);
+			var freeze2CmdId = new CommandID(GuidList.guidWordLightPackageCmdSet, PkgCmdIDList.cmdidFreezeSearch2);
+			var freeze3CmdId = new CommandID(GuidList.guidWordLightPackageCmdSet, PkgCmdIDList.cmdidFreezeSearch3);
+			var enableLog3CmdId = new CommandID(GuidList.guidWordLightPackageCmdSet, PkgCmdIDList.cmdidEnableLog);
+
 			var mcs = GetService(typeof(IMenuCommandService)) as IMenuCommandService;
 			if (mcs != null)
 			{
-				// Create the command for the menu item.
-				CommandID menuCommandID =
-					new CommandID(GuidList.guidWordLightPackageCmdSet, (int)PkgCmdIDList.cmdidSettings);
-				MenuCommand menuItem = new MenuCommand(SettingsMenuItemExecuted, menuCommandID);
-				mcs.AddCommand(menuItem);
+				mcs.AddCommand(new MenuCommand(SettingsMenuItemExecuted, settingsCmdId));
 
-				CommandID menuCommandID2 =
-					new CommandID(GuidList.guidWordLightPackageCmdSet, (int)PkgCmdIDList.cmdidFreezeSearch1);
-				MenuCommand menuItem2 = new MenuCommand((o,e) => FreezeSearchGroup(1), menuCommandID2);
-				mcs.AddCommand(menuItem2);
+				mcs.AddCommand(new MenuCommand((o, e) => FreezeSearchGroup(1), freeze1CmdId));
+				mcs.AddCommand(new MenuCommand((o, e) => FreezeSearchGroup(2), freeze2CmdId));
+				mcs.AddCommand(new MenuCommand((o, e) => FreezeSearchGroup(3), freeze3CmdId));
+
+				var mc = new MenuCommand((o, e) => Log.Enabled = true, enableLog3CmdId);
+				mcs.AddCommand(mc);	
 			}
 		}
 
@@ -179,149 +162,11 @@ namespace WordLight2010
 
 		private void FreezeSearchGroup(int groupIndex)
 		{
-			_watcher.FreezeSearchOnActiveTextView(groupIndex);
-		}
-
-
-		#region Command registration
-
-		private void RegisterCommands()
-		{
-			if (_dte == null)
+			if (_watcher != null)
 			{
-				return;
-			}
-
-			Commands commands = (Commands)_dte.Commands;
-			
-			object[] contextGUIDS = new object[] { };
-
-			//This try/catch block can be duplicated if you wish to add multiple commands to be handled by your Add-in,
-			//  just make sure you also update the QueryStatus/Exec method to include the new command names.
-			try
-			{
-				RegisterSimpleCommand(commands, Freeze1CommandName, "Freeze search 1", AddinSettings.Instance.FreezeMark1Hotkey);
-				RegisterSimpleCommand(commands, Freeze2CommandName, "Freeze search 2", AddinSettings.Instance.FreezeMark2Hotkey);
-				RegisterSimpleCommand(commands, Freeze3CommandName, "Freeze search 3", AddinSettings.Instance.FreezeMark3Hotkey);
-
-				RegisterSimpleCommand(commands, EnableLogCommandName, "Enable log", null);
-			}
-			catch (System.ArgumentException)
-			{
-				//If we are here, then the exception is probably because a command with that name
-				//  already exists. If so there is no need to recreate the command and we can 
-				//  safely ignore the exception.
+				_watcher.FreezeSearchOnActiveTextView(groupIndex);
 			}
 		}
 
-		private void RegisterSimpleCommand(Commands commands, string name, string buttonText, string bindings)
-		{
-
-			//object[] contextGUIDS = new object[] { };
-			//Command cmd = commands.AddNamedCommand(_dte, name, buttonText, string.Empty, true, 0, null, 16);
-
-			//if (bindings != null)
-			//{
-			//    cmd.Bindings = bindings;
-			//}
-		}
-
-		private void RemoveCommands()
-		{
-			//List<Command> toDelete = new List<Command>();
-
-			//foreach (Command cmd in _application.Commands)
-			//{
-			//    if (!string.IsNullOrEmpty(cmd.Name) && cmd.Name.StartsWith(_addInInstance.ProgID + "."))
-			//        toDelete.Add(cmd);
-			//}
-
-			//foreach (Command cmd in toDelete)
-			//{
-			//    cmd.Delete();
-			//}
-		}
-
-		#endregion
-
-
-		#region Implements IDTCommandTarget
-
-		/// <summary>Implements the QueryStatus method of the IDTCommandTarget interface. This is called when the command's availability is updated</summary>
-		/// <param term='commandName'>The name of the command to determine state for.</param>
-		/// <param term='neededText'>Text that is needed for the command.</param>
-		/// <param term='status'>The state of the command in the user interface.</param>
-		/// <param term='commandText'>Text requested by the neededText parameter.</param>
-		/// <seealso class='Exec' />
-		public void QueryStatus(string commandName, vsCommandStatusTextWanted neededText, ref vsCommandStatus status, ref object commandText)
-		{
-			//try
-			//{
-			//    if (neededText == vsCommandStatusTextWanted.vsCommandStatusTextWantedNone)
-			//    {
-			//        string commandPrefix = _addInInstance.ProgID + ".";
-
-			//        if (commandName == commandPrefix + Freeze1CommandName ||
-			//            commandName == commandPrefix + Freeze2CommandName ||
-			//            commandName == commandPrefix + Freeze3CommandName ||
-			//            commandName == commandPrefix + EnableLogCommandName)
-			//        {
-			//            status = (vsCommandStatus)vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
-			//            return;
-			//        }
-			//    }
-			//}
-			//catch (Exception ex)
-			//{
-			//    Log.Error("Unhandled exception in IDTCommandTarget.QueryStatus", ex);
-			//}
-		}
-
-		/// <summary>Implements the Exec method of the IDTCommandTarget interface. This is called when the command is invoked.</summary>
-		/// <param term='commandName'>The name of the command to execute.</param>
-		/// <param term='executeOption'>Describes how the command should be run.</param>
-		/// <param term='varIn'>Parameters passed from the caller to the command handler.</param>
-		/// <param term='varOut'>Parameters passed from the command handler to the caller.</param>
-		/// <param term='handled'>Informs the caller if the command was handled or not.</param>
-		/// <seealso class='Exec' />
-		public void Exec(string commandName, vsCommandExecOption executeOption, ref object varIn, ref object varOut, ref bool handled)
-		{
-			handled = false;
-
-			//try
-			//{
-			//    if (executeOption == vsCommandExecOption.vsCommandExecOptionDoDefault)
-			//    {
-			//        string commandPrefix = _addInInstance.ProgID + ".";
-
-			//        if (commandName == commandPrefix + EnableLogCommandName)
-			//        {
-			//            Log.Enabled = true;
-			//            handled = true;
-			//        }
-			//        else if (commandName == commandPrefix + Freeze1CommandName)
-			//        {
-			//            FreezeSearchGroup(1);
-			//            handled = true;
-			//        }
-			//        else if (commandName == commandPrefix + Freeze2CommandName)
-			//        {
-			//            FreezeSearchGroup(2);
-			//            handled = true;
-			//        }
-			//        else if (commandName == commandPrefix + Freeze3CommandName)
-			//        {
-			//            FreezeSearchGroup(3);
-			//            handled = true;
-			//        }
-			//    }
-			//}
-			//catch (Exception ex)
-			//{
-			//    Log.Error("Unhandled exception in IDTCommandTarget.Exec", ex);
-			//}
-		}
-
-		#endregion
 	}
 }
